e57867bb83fa93f39e36764cbf58b1e241f8b177
Martin Pitt <martin@amutable.com>
2026-09-23 11:18:15 +0000

    rpm: Add the missing SPDX header to the planner test
    The file landed in 8cdc576 in parallel with 0f310be8a4a45.
    
    Signed-off-by: Martin Pitt <martin@amutable.com>

--- package_system/rpm/plan_test.py
+++ package_system/rpm/plan_test.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Amutable GmbH <https://amutable.com/>
+# SPDX-License-Identifier: MPL-2.0
+
 """Tests for the planner against real repodata and a real solve.
 
 Builds throwaway packages and indexes them with the same driver a build uses, thus exercises
